c36483b62fb883b823314206d85ccb5bc488cf01
akhila1005 <chiguluri.a@northeastern.edu>
2023-09-10 17:56:32 -0400

    commit 1

---
+++ NUMAD23Fa_AkhilaChiguluri/app/src/main/java/edu/northeastern/numad23fa_akhilachiguluri/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.northeastern.numad23fa_akhilachiguluri;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button aboutMe = findViewById(R.id.aboutMe);
+        aboutMe .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String my_name = "Akhila Chiguluri";
+                String my_emailId = "chiguluri.a@northeastern.edu";
+                String message_to_be_toasted = "Name: " + my_name + "\nEmail Id: "+ my_emailId;
+                Toast.makeText(MainActivity.this, message_to_be_toasted, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
